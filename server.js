2d0aaf18fc407c3d5ba87bfca75e85ef27d3b416
yaroslavbilokin <yaroslavbilokin41@gmail.com>
2020-06-12 19:00:14 +0300

    added search book and author by id

--- server.js
+++ server.js
@@ -73,6 +73,14 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A Single Book',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id)
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of All Books',
@@ -82,6 +90,14 @@ const RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(AuthorType),
             description: 'List of All Authors',
             resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: 'A single Author',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id)
         }
     })
 })
